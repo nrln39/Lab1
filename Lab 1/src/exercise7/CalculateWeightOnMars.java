6f50ea6e6e56179335c0966efedbaf9a910cc8b2
Nurul Ain Fatehah <nurulainmohdsham00@gmail.com>
2021-03-18 14:09:50 +0800

    change formula of weight on mars

--- Lab 1/src/exercise7/CalculateWeightOnMars.java
+++ Lab 1/src/exercise7/CalculateWeightOnMars.java
@@ -3,8 +3,9 @@ import java.util.*;
 
 public class CalculateWeightOnMars {
 	private double weight;
-	static double marsWeight = 2.20462 * 0.38;
-	// kg to lbs = 1 * 2.20462 && weight on earth is 0.38 heavier on mars
+	static double marsWeight = 0.3783;
+	// weight on earth is 0.6217 heavier than on mars
+	// weight on mars is 0.3783 lighter than on earth
 	
 	
 	public double getWeight() 
