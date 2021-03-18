f76edd80894f9205ceed70da72c9504e662dc7b2
Nurul Ain Fatehah <nurulainmohdsham00@gmail.com>
2021-03-18 14:14:16 +0800

    added comments on lines of code

--- Lab 1/src/exercise7/CalculateWeightOnMars.java
+++ Lab 1/src/exercise7/CalculateWeightOnMars.java
@@ -2,7 +2,7 @@ package exercise7;
 import java.util.*;
 
 public class CalculateWeightOnMars {
-	private double weight;
+	private double weight; //attr. weight to hold value passed from console
 	static double marsWeight = 0.3783;
 	// weight on earth is 0.6217 heavier than on mars
 	// weight on mars is 0.3783 lighter than on earth
@@ -11,11 +11,13 @@ public class CalculateWeightOnMars {
 	public double getWeight() 
 	{
 		return weight;
+		//return value stored to main method
 	}
 	
 	public void setWeight(double weight)
 	{
 		this.weight = weight * marsWeight;
+		// store value received from main method
 	}
 	
 	
