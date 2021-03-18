558925e7e3486f5f62494724b4d172ae4651c26c
Nurul Ain Fatehah <nurulainmohdsham00@gmail.com>
2021-03-18 15:17:08 +0800

    loop to get to the next person is constructed

--- Lab 1/src/exercise8/Human.java
+++ Lab 1/src/exercise8/Human.java
@@ -25,26 +25,42 @@ public class Human {
 		CalculateWeightOnMars mars = new CalculateWeightOnMars();
 		// initialize object for class CalculateWeightOnMars
 		
+		Human human = new Human();
+		
 		Scanner print = new Scanner(System.in);
 		// initialize object for package scanner for display purpose
 		
 		int choice = 0;
+		int counter = 0;
 		
 		do
 		{
-			
+			counter++;
+			System.out.println();
+			System.out.println("===============  Person " + counter + "  ===============");
+			System.out.println();
 			System.out.println("Please enter your name: "); // ask user for name
+			name = print.nextLine();
+			
+			human.setName(name);
+			
+			System.out.println();
+			
 			
 			System.out.println("Please enter your weight: (kg)"); // ask user for weight in kilograms
 			weight = print.nextDouble(); // read input on console
 			mars.setWeight(weight); // send value from attr. weight to method setWeight from class CalculateWeightOnMars
 			
-			
+			System.out.println();
 			System.out.println("Enter 1 to enter weight of next person.");
+			choice = print.nextInt();
+			print.nextLine();
+			
 		}while(choice == 1);
 		
 		
 		
+		System.out.println();
 		System.out.println("The list of information of people's weight would be as follows: ");
 		
 	}
