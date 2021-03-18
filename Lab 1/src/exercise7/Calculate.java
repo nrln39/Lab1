806f280de6011c47cd490d90c1bbe982be0ac10b
Nurul Ain Fatehah <nurulainmohdsham00@gmail.com>
2021-03-18 14:18:28 +0800

    Added comments on main method from class Calculate

--- Lab 1/src/exercise7/Calculate.java
+++ Lab 1/src/exercise7/Calculate.java
@@ -7,13 +7,18 @@ public class Calculate {
 	public static void main(String[] args) {
 		
 		CalculateWeightOnMars mars = new CalculateWeightOnMars();
+		// initialize object for class CalculateWeightOnMars
+		
 		double weight;
+		// attr. weight to store weight
+		
 		Scanner print = new Scanner(System.in);
+		// initialize object for package scanner for display purpose
 		
 		
-		System.out.println("Please enter your weight: (kg)");
-		weight = print.nextDouble();
-		mars.setWeight(weight);
+		System.out.println("Please enter your weight: (kg)"); // ask user for weight in kilograms
+		weight = print.nextDouble(); // read input on console
+		mars.setWeight(weight); // send value from attr. weight to method setWeight from class CalculateWeightOnMars
 		
 		System.out.println();
 		System.out.println("------------------------------------------");
@@ -21,7 +26,7 @@ public class Calculate {
 		System.out.println("------------------------------------------");
 		System.out.println();
 		System.out.println("     " + weight + "       |" + "        " + Math.round(mars.getWeight() * 100.0) / 100.0);
-		
+		// display the comparison between weight on earth and mars. weight on mars is rounded off 2 decimal place 
 		System.out.println("------------------------------------------");
 		
     }
