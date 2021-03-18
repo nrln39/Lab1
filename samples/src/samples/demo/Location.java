4e732a33c483ac8d6895937758323d677eb4ff0b
Nurul Ain Fatehah <nurulainmohdsham00@gmail.com>
2021-03-18 12:57:57 +0800

    Location Class 1st Commit

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,17 @@
+package samples.demo;
+
+
+
+public class Location {
+	private String loc;
+	
+	public String getLocation()
+	{
+		return loc;
+	}
+	
+	public void setLocation(String loc)
+	{
+		this.loc = loc;
+	}
+}
