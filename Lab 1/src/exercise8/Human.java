8371c37841c0093a0baa17c750492a9ddfdc5231
Nurul Ain Fatehah <nurulainmohdsham00@gmail.com>
2021-03-18 14:57:26 +0800

    Human class initial commit

---
+++ Lab 1/src/exercise8/Human.java
@@ -0,0 +1,34 @@
+package exercise8;
+import java.util.Scanner;
+
+import exercise7.CalculateWeightOnMars;
+
+public class Human {
+	private String name;
+	
+	private String getName(String name)
+	{
+		return name;
+	}
+	
+	private void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		String name; // attr. to hold value name
+		double weight; // attr. to hold value weight
+		
+		CalculateWeightOnMars mars = new CalculateWeightOnMars();
+		// initialize object for class CalculateWeightOnMars
+		
+		Scanner print = new Scanner(System.in);
+		// initialize object for package scanner for display purpose
+	}
+	
+	
+}
+
