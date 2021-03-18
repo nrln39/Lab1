995d7b010ae80766523ba71093f9232058b0eb39
Nurul Ain Fatehah <nurulainmohdsham00@gmail.com>
2021-03-18 13:49:31 +0800

    done with setter/ getter

--- Lab 1/src/exercise7/CalculateWeightOnMars.java
+++ Lab 1/src/exercise7/CalculateWeightOnMars.java
@@ -1,8 +1,25 @@
 package exercise7;
+import java.util.*;
 
 public class CalculateWeightOnMars {
 	private double weight;
 	static double marsWeight = 2.20462 * 0.38;
+	// kg to lbs = 1 * 2.20462 && weight on earth is 0.38 heavier on mars
 	
 	
+	public double getWeight() 
+	{
+		return weight;
+	}
+	
+	public void setWeight(double weight)
+	{
+		this.weight = weight * marsWeight;
+	}
+	
+	public static void main(String[] args) {
+		Scanner print = new Scanner(System.in);
+		System.out.println("Please enter your weight: (kg)");
+		
+    }
 }
